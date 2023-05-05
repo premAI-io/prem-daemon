9c40cfeaa89e148b0341996b178473136f998f65
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-05-05 15:35:12 +0200

    controller logic

--- app/core/events.py
+++ app/core/events.py
@@ -3,6 +3,7 @@ from collections.abc import Callable
 
 from fastapi import FastAPI
 
+from app.core import config
 from app.core.utils import check_model_ready, download_model, load_model
 
 logger = logging.getLogger(__name__)
@@ -10,9 +11,10 @@ logger = logging.getLogger(__name__)
 
 def create_start_app_handler(app: FastAPI) -> Callable:
     def start_app() -> None:
-        ready, _ = check_model_ready()
-        if not ready:
-            download_model()
-        load_model()
+        if config.MODEL_ID != "controller":
+            ready, _ = check_model_ready()
+            if not ready:
+                download_model()
+            load_model()
 
     return start_app
