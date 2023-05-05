9a30b6f3ef5f3ac0f72f8607a40fa5e3a70c70cd
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-05-05 15:41:15 +0200

    fixed tests for controller

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -5,7 +5,8 @@ from app.core import config
 from app.core.utils import get_model_info
 
 router = APIRouter()
-router.include_router(info.router, tags=["info"], prefix="/v1")
 
-if "chat" in get_model_info(config.MODEL_ID).get("modelTypes"):
+if config.MODEL_ID == "controller":
+    router.include_router(info.router, tags=["info"], prefix="/v1")
+elif "chat" in get_model_info(config.MODEL_ID).get("modelTypes"):
     router.include_router(chat.router, tags=["chat"], prefix="/v1")
