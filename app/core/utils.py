9ecf42c1aade89991040a52df0edb2b0589e1eec
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-05-23 18:58:21 +0200

    added documentation to apps

--- app/core/utils.py
+++ app/core/utils.py
@@ -20,8 +20,21 @@ APPS = [
         "name": "Prem Embeddings",
         "playground": False,
         "documentation": """
-        # Prem Chat
-        """,
+        # Prem Embeddings
+
+        ## Description
+
+        Prem Embeddings are all the services used to transform unstructured text in a vector representation. A vector representation is a vector of numbers that represents the most important features of the text. For example, a sentence can be represented as a vector of numbers. The vector is obtained using a neural network that is trained to extract the most important features of the sentence. Embeddings are used in many NLP tasks, such as text classification, text clustering, text similarity, and so on. In order to give memory to ChatGPT, we need to transform the text in a vector representation and store them in a vectorstore for later retrieval.
+
+        ## Installation & Usage
+
+        All the services compatible with Prem Embeddings interface expose an API with the following endpoints:
+
+        - `/api/v1/embeddings/`
+
+        Check the OpenAPI documentation at the link http://{service}:{port}/docs for more information.
+
+        """,  # noqa E501
         "icon": "/assets/apps/embeddings.svg",
     },
     {
@@ -29,8 +42,16 @@ APPS = [
         "name": "Prem Store",
         "playground": False,
         "documentation": """
-        # Prem Chat
-        """,
+        # Prem Store
+
+        ## Description
+
+        Prem Store are all the services that expose a vector database. A vector database is used to store embeddings. An embedding is a vector representation of a piece of data. For example, a sentence can be represented as a vector of numbers. The vector is obtained using a neural network that is trained to extract the most important features of the sentence.
+
+        ## Installation & Usage
+
+        We don't have a standard interface for what concerns Prem Store. However, we suggest to connect with Prem Store services using Langchain python library. You can find the library [here](https://python.langchain.com/en/latest/index.html).
+        """,  # noqa E501
         "icon": "/assets/apps/store.svg",
     },
     {
@@ -38,8 +59,24 @@ APPS = [
         "name": "Prem Copilot",
         "playground": False,
         "documentation": """
-        # Prem Chat
-        """,
+        # Prem Copilot
+
+        All the services exposed using Prem Copilot interface can be used using the official Github Copilot extension. You can find the extension [here](https://marketplace.visualstudio.com/items?itemName=GitHub.copilot).
+
+        ## Installation
+
+        In the `settings.json` file of your VSCode, add the following lines:
+
+        ```json
+            "github.copilot.advanced": {
+            "debug.overrideEngine": "codegen",
+            "debug.testOverrideProxyUrl": "http://localhost:18080",
+            "debug.overrideProxyUrl": "http://localhost:18080"
+        }
+        ```
+
+        > Make sure to point to the correct port of the Prem Copilot service.
+        """,  # noqa E501
         "icon": "/assets/apps/copilot.svg",
     },
     {
@@ -47,7 +84,7 @@ APPS = [
         "name": "Prem Michelangelo",
         "playground": True,
         "documentation": """
-        # Prem Chat
+        # Prem Michelangelo
         """,
         "icon": "/assets/apps/michelangelo.svg",
     },
