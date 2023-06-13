8e604ba980b44c1d77e4e39bcfebe2271d1c525f
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-06-13 17:58:26 +0200

    few renames

--- app/core/utils.py
+++ app/core/utils.py
@@ -55,11 +55,11 @@ doc_result = embeddings.embed_documents([text])
         "icon": "/assets/apps/embeddings.svg",
     },
     {
-        "id": "store",
-        "name": "Prem Store",
+        "id": "vector-store",
+        "name": "Prem Vector Store",
         "playground": False,
         "documentation": """
-# Prem Store
+# Prem Vector Store
 
 ## Description
 
@@ -97,13 +97,13 @@ In the `settings.json` file of your VSCode, add the following lines:
         "icon": "/assets/apps/copilot.svg",
     },
     {
-        "id": "diffusion",
-        "name": "Prem Diffusion",
+        "id": "diffuser",
+        "name": "Prem Diffuser",
         "playground": False,
         "documentation": """
-        # Prem Diffusion
+        # Prem Diffuser
         """,
-        "icon": "/assets/apps/diffusion.svg",
+        "icon": "/assets/apps/diffuser.svg",
     },
     {
         "id": "text-to-audio",
