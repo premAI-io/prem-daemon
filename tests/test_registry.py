bd432818a329506cc80f78dcce45e63183fef76e
nsosio <sosio.nicola94@tiscali.it>
2023-07-31 20:59:19 +0200

    bugfix in tests

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -76,18 +76,18 @@ class TestController:
         assert response.status_code == 200
         assert len(response.json()) == number_of_services + 1
 
-    def test_add_existing_service(self) -> None:
-        response = self.client.get("/v1/services/")
+    def test_add_existing_service(self, client) -> None:
+        response = client.get("/v1/services/")
         assert response.status_code == 200
         number_of_services = len(response.json())
 
-        response = self.client.post(
+        response = client.post(
             "/v1/services/",
             json=response.json()[-1],
         )
         assert response.status_code != 200
 
-        response = self.client.get("/v1/services/")
+        response = client.get("/v1/services/")
         assert response.status_code == 200
         assert len(response.json()) == number_of_services
 
